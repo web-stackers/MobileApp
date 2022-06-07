74c1b6f4f3e55e8f992ee259a2344da6a51123bd
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-06-07 16:28:25 +0530

    Created separate file for Consumer

--- client/index.js
+++ client/index.js
@@ -3,7 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
+import Consumer from './Consumer';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Consumer);
