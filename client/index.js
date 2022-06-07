1e42107c647dcaf102103a6628b77f6b31460ee0
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-06-07 23:36:16 +0530

    ConsumerForm started to develop

--- client/index.js
+++ client/index.js
@@ -1,11 +1,9 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import Consumer from './Consumer';
 import App from './App';
+import ConsumerForm from './screens/ConsumerForm';
 import {name as appName} from './app.json';
 
 // AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => Consumer);
+// AppRegistry.registerComponent(appName, () => Consumer);
+AppRegistry.registerComponent(appName, () => ConsumerForm);
---
+++ client/screens/ConsumerForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+import Header from '../components/Header';
+
+const ConsumerForm = () => {
+  return (
+    <View>
+      <Header title="Search for provider"></Header>
+    </View>
+  );
+};
+
+export default ConsumerForm;
