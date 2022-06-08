13f6545ae135ca691e185b80cc754a90375ef21e
Pavadaran <pavadaran17@gmail.com>
2022-06-08 20:59:21 +0530

    Deleted server folder

--- client/index.js
+++ client/index.js
@@ -1,10 +1,9 @@
 import {AppRegistry} from 'react-native';
-import Consumer from './Consumer';
 import Provider from './Provider';
 import App from './App';
 import ConsumerForm from './screens/consumer/ConsumerForm';
 import {name as appName} from './app.json';
 
 // AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => Consumer);
-/* AppRegistry.registerComponent(appName, () => Provider); */
+/* AppRegistry.registerComponent(appName, () => Consumer); */
+AppRegistry.registerComponent(appName, () => Provider);
