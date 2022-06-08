7d5cf71708bb03a976547288160f59afbb1e5626
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-06-08 20:02:17 +0530

    deleted server

--- client/Provider/screens/JobResponse/Acknowledge/index.js
+++ client/Provider/screens/JobResponse/Acknowledge/index.js
@@ -1,37 +1,30 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import {View, Text, Image} from 'react-native';
 
 import styles from './styles';
 import Sbutton from '../../../../components/Sbutton';
 
-const Acknowledge = ({ route, navigation}) => {
+const Acknowledge = ({route, navigation}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.message}>
+        <Text style={styles.title}>{route.params.title}</Text>
+        <Image
+          style={styles.tick}
+          source={require('../../../../assets/images/tick.png')}
+        />
+        <Text style={styles.subtitle}>{route.params.subtitle}</Text>
+      </View>
 
-    return ( 
-        <View style={styles.container}>
-            <View style={styles.message}>
-                <Text style={styles.title}>
-                    {route.params.title}
-                </Text>
-                <Image
-                    style={styles.tick}
-                    source={require('../../../../assets/images/tick.png')}
-                />
-                <Text style={styles.subtitle}>
-                    {route.params.subtitle}
-                </Text>
-            </View>
+      <View style={styles.doneButton}>
+        <Sbutton
+          type="primary"
+          text="Done"
+          onPress={() => navigation.navigate('Job Details')}
+        />
+      </View>
+    </View>
+  );
+};
 
-            <View style={styles.doneButton}>
-                <Sbutton 
-                    type='primary' 
-                    text='Done'
-                    onPress={() => navigation.navigate('Job Details')}
-                />
-            </View>
-        </View>
-     );
-}
- 
 export default Acknowledge;
-
-
