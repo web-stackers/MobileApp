6bc2a808a0620ab2e52ceb4aba7dd58a52c4e27f
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-06-10 19:52:15 +0530

    uncommented provider

--- client/index.js
+++ client/index.js
@@ -1,6 +1,5 @@
 import {AppRegistry} from 'react-native';
-// import QuotationDetails from './consumer/screens/JobRequest/QuotationDetails';
-// import Provider from './Provider';
+import Provider from './Provider';
 import {Provider as PaperProvider} from 'react-native-paper';
 import * as React from 'react';
 import Consumer from './Consumer';
@@ -14,7 +13,5 @@ export default function Main() {
     </PaperProvider>
   );
 }
-// AppRegistry.registerComponent(appName, () => App);
-// AppRegistry.registerComponent(appName, () => QuotationDetails);
-// AppRegistry.registerComponent(appName, () => Provider);
+
 AppRegistry.registerComponent(appName, () => Main);
