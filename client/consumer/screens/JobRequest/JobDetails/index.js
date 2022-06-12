e48b9ad0846f4f46ce6510aeede27c33b981b789
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-06-12 08:42:31 +0530

    date picker

--- client/consumer/screens/JobRequest/JobDetails/index.js
+++ client/consumer/screens/JobRequest/JobDetails/index.js
@@ -3,12 +3,34 @@ import {Text, View, SafeAreaView, Alert} from 'react-native';
 
 import styles from './styles';
 import Sbutton from '../../../../components/Sbutton';
-import Sdate from '../../../../components/FormComponents/Sdate';
 import StextInput from '../../../../components/FormComponents/StextInput';
 import Sheader from '../../../../components/Sheader';
 import Sselect from '../../../../components/FormComponents/Sselect';
 
+import RNDateTimePicker from '@react-native-community/datetimepicker';
 const JobDetails = ({navigation}) => {
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const showMode = currentMode => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
   const [jobType, setJobType] = useState('');
 
   const jobList = [
@@ -64,7 +86,26 @@ const JobDetails = ({navigation}) => {
       </SafeAreaView>
       <Text style={styles.text}>When you want the work to be done?</Text>
 
-      <Sdate title="Select Job Requesting Date and Time" />
+      <View>
+        <View>
+          <Sbutton onPress={showDatepicker} text="Show date picker" />
+        </View>
+        <View>
+          <Sbutton onPress={showTimepicker} text="Show time picker" />
+        </View>
+        <Text style={styles.text}>selected: {date.toLocaleString()}</Text>
+        {show && (
+          <RNDateTimePicker
+            themeVariant="dark"
+            textColor="red"
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={true}
+            onChange={onChange}
+          />
+        )}
+      </View>
 
       <View style={styles.btngrp}>
         <Sbutton primary={true} text="Search Provider" onPress={handleSubmit} />
