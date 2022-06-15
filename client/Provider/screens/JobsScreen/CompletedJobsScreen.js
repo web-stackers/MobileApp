8cc6eddcf17c954fad1c2d97e601c8dabf257c6d
Vellummyilum Vinoth <86709705+Windyvinoth13@users.noreply.github.com>
2022-06-15 22:04:42 +0530

    Update CompletedJobsScreen.js

--- client/Provider/screens/JobsScreen/CompletedJobsScreen.js
+++ client/Provider/screens/JobsScreen/CompletedJobsScreen.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View, Text, FlatList, SafeAreaView} from 'react-native';
-import styles from '../../../components/Styles';
 import {List, Button} from 'react-native-paper';
 
 const CompletedJobsScreen = ({navigation}) => {
