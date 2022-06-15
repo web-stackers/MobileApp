fce588da41640b1dc42c0406cff2baf3cd563d23
Vellummyilum Vinoth <86709705+Windyvinoth13@users.noreply.github.com>
2022-06-15 16:51:41 +0530

    Update Provider.js

--- client/Provider.js
+++ client/Provider.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
-import JobStackScreen from './provider/stacks/JobStack';
-// import BottomNavigator from './provider/stacks/BottomNavigator';
+import JobStackScreen from './Provider/stacks/JobStack';
+import BottomNavigator from './Provider/stacks/BottomNavigator';
 
 const Provider = () => {
   return (
