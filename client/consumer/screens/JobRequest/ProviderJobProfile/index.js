e755cfc085161b1b99000303da521d11dec8544f
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-06-15 09:13:34 +0530

    added some styling in provider profile

--- client/consumer/screens/JobRequest/ProviderJobProfile/index.js
+++ client/consumer/screens/JobRequest/ProviderJobProfile/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, ScrollView} from 'react-native';
 
 import styles from './styles';
 import Sbutton from '../../../../components/Sbutton';
 
 const ProviderJobProfile = ({navigation}) => {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Name</Text>
+        <Image
+          style={styles.image}
+          source={require('../../../../assets/images/profile.jpg')}
+        />
+      </View>
+      <View>
+        <Text style={styles.subtitle}>Work Started Year</Text>
+        <Text style={styles.subtitle}>Qualification</Text>
+        <Text style={styles.subtitle}>Rating</Text>
+      </View>
+
       <View style={styles.btngrp}>
         <Sbutton primary={true} text="Send Request" />
         <Sbutton text="Go Back" />
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
--- client/consumer/screens/JobRequest/ProviderJobProfile/styles.js
+++ client/consumer/screens/JobRequest/ProviderJobProfile/styles.js
@@ -4,8 +4,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121212',
-    alignItems: 'center',
-    justifyContent: 'center',
     width: '100%',
     height: Dimensions.get('window').height,
   },
@@ -14,6 +12,27 @@ const styles = StyleSheet.create({
     marginBottom: 1,
     marginTop: 20,
     alignSelf: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 35,
+    marginBottom: 50,
+    marginTop: 50,
+  },
+  subtitle: {
+    color: '#fff',
+    fontSize: 20,
+    marginBottom: 10,
+    marginTop: 10,
+    marginLeft: 20,
+  },
+  content: {
+    alignItems: 'center',
+  },
+  image: {
+    height: 250,
+    width: 250,
+    marginBottom: 55,
   },
 });
 
