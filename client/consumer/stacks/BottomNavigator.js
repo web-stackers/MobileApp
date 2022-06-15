a2d0dfbfde0c0c004e6abdeb1d6a37346b3f121a
Vellummyilum Vinoth <86709705+Windyvinoth13@users.noreply.github.com>
2022-06-15 16:53:11 +0530

    Update BottomNavigator.js

--- client/consumer/stacks/BottomNavigator.js
+++ client/consumer/stacks/BottomNavigator.js
@@ -1,43 +1,55 @@
-// import * as React from 'react';
-// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-// import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import * as React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-// import HomeScreen from '../screens/HomeScreen';
-// import SettingStack from './SettingStack';
+import HomeScreen from '../screens/HomeScreen';
+import SettingStack from './SettingStack';
+import JobRequestStackScreen from './JobRequestStack';
 
-// const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
-// const BottomNavigator = () => {
-//   return (
-//     <Tab.Navigator
-//       initialRouteName="Home"
-//       screenOptions={{
-//         tabBarActiveTintColor: '#652C9E',
-//         backgroundColor: '#652C9E',
-//       }}>
-//       <Tab.Screen
-//         name="Home"
-//         component={HomeScreen}
-//         options={{
-//           tabBarLabel: 'Home',
-//           tabBarIcon: ({color, size}) => (
-//             <MaterialCommunityIcons name="home" color={color} size={size} />
-//           ),
-//         }}
-//       />
-//       <Tab.Screen
-//         name="Profile"
-//         component={SettingStack}
-//         options={{
-//           tabBarLabel: 'Profile',
-//           tabBarIcon: ({color, size}) => (
-//             <MaterialCommunityIcons name="account" color={color} size={size} />
-//           ),
-//           headerShown: false,
-//         }}
-//       />
-//     </Tab.Navigator>
-//   );
-// };
+const BottomNavigator = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: '#652C9E',
+        backgroundColor: '#652C9E',
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Jobs"
+        component={JobRequestStackScreen}
+        options={{
+          tabBarLabel: 'Jobs',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="table-search" color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={SettingStack}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
-// export default BottomNavigator;
+export default BottomNavigator;
