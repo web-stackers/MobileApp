8c2004289a29ae4eda311838fb3e82e1e2f8e549
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-06-15 17:03:00 +0530

    commented vinoth part

--- client/index.js
+++ client/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable prettier/prettier */
 import {AppRegistry} from 'react-native';
-import Provider from './Provider';
+// import Provider from './Provider';
 import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import * as React from 'react';
 import Consumer from './Consumer';
-import Provider from './Provider';
+
 import App from './App';
 import {name as appName} from './app.json';
 import {enableLatestRenderer} from 'react-native-maps';
@@ -29,7 +29,7 @@ const theme = {
 export default function Main() {
   return (
     <PaperProvider theme={theme}>
-      <Provider />
+      <Consumer />
     </PaperProvider>
   );
 }
