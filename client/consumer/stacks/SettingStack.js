9ad54e5e096618bb09f62fa149a741626c787b7c
Vellummyilum Vinoth <86709705+Windyvinoth13@users.noreply.github.com>
2022-06-15 22:03:30 +0530

    Update SettingStack.js

--- client/consumer/stacks/SettingStack.js
+++ client/consumer/stacks/SettingStack.js
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
