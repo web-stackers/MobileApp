e1a8683bdd2908094926dc2289b41f4d3db10d70
Pavadaran <pavadaran17@gmail.com>
2022-06-16 20:14:55 +0530

    Deleted error files

--- client/common/stacks/RatingsAndReviewsStack.js
+++ client/common/stacks/RatingsAndReviewsStack.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import RatingsAndReviews from '../screens/RatingsAndReviews';
+import RatingsAndReviews from '../screens/ratingsAndReviews';
 
 const RatingsAndReviewsStack = createNativeStackNavigator();
 
