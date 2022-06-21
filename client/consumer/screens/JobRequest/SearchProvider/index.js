68990570002c34f1a8a3367448ea7c40728dab5d
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-06-21 09:22:05 +0530

    distance calculator

--- client/consumer/screens/JobRequest/SearchProvider/index.js
+++ client/consumer/screens/JobRequest/SearchProvider/index.js
@@ -13,86 +13,107 @@ const SearchProvider = ({navigation, route}) => {
   const {jobType, description, requestedTime, providersList, lat, longi, CID} =
     route.params;
 
+  const lat1 = lat / 57.29577951;
+  const longi1 = longi / 57.29577951;
   // const [documentList, setDocumentList] = useState([]);
 
-  const AlertRequest = id =>
-    axios
-      .get(`http://10.0.2.2:5000/job/availability/${requestedTime}/${id}`)
-      .then(response => {
-        console.log(
-          'Check whether provider availability is checked..............................................................',
-        );
-        console.log(response.data);
-        if (!response.data) {
-          Alert.alert(
-            'Sending Request',
-            'Are you sure to send the request for that provider?',
-            [
-              {
-                text: 'Cancel',
-                onPress: () => console.log('Cancel Pressed'),
-                style: 'cancel',
-              },
-              {
-                text: 'OK',
-                onPress: () => {
-                  console.log('provider Id');
-                  console.log(id);
-
-                  axios
-                    .post('http://10.0.2.2:5000/job', {
-                      jobType: jobType,
-                      description: description,
-                      requestedTime: requestedTime,
-                      longitude: longi,
-                      latitude: lat,
-                      providerId: id,
-                      consumerId: CID,
-                    })
-                    .then(function (response) {
-                      console.log(response.data);
-                      console.log(response.data._id);
-                      axios
-                        .post('http://10.0.2.2:5000/jobAssignment', {
-                          jobId: response.data._id,
-                          providerId: id,
-                        })
-                        .then(function (response) {
-                          console.log(response.data);
-                          navigation.navigate('PhotoUpload', {
-                            jobId: response.data.jobId,
-                          });
-                          // navigation.navigate('JobAcknowledge');
-                        })
-                        .catch(function (error) {
-                          console.log(error);
-                        });
-                    })
-                    .catch(function (error) {
-                      console.log(error);
-                    });
+  const AlertRequest = (id, providerLat, providerLongi) => {
+    let lat2 = providerLat / 57.29577951;
+    let longi2 = providerLongi / 57.29577951;
+    let distance =
+      3963.0 *
+      Math.acos(
+        Math.sin(lat1) * Math.sin(lat2) +
+          Math.cos(lat1) * Math.cos(lat2) * Math.cos(longi2 - longi1),
+      );
+    console.log(
+      'Distance between two places.................................................',
+    );
+    console.log(distance);
+    if (distance < 15) {
+      axios
+        .get(`http://10.0.2.2:5000/job/availability/${requestedTime}/${id}`)
+        .then(response => {
+          console.log(
+            'Check whether provider availability is checked..............................................................',
+          );
+          console.log(response.data);
+          if (!response.data) {
+            Alert.alert(
+              'Sending Request',
+              'Are you sure to send the request for that provider?',
+              [
+                {
+                  text: 'Cancel',
+                  onPress: () => console.log('Cancel Pressed'),
+                  style: 'cancel',
                 },
-              },
-            ],
-          );
-        } else {
-          Alert.alert(
-            'Provider not available',
-            'We are sorry to inform that, your requested provider will not be available on your requested date. You can able to make schedule on another day',
-            [
-              {
-                text: 'Cancel',
-                onPress: () => console.log('Cancel Pressed'),
-                style: 'cancel',
-              },
-              {
-                text: 'Request another date',
-                onPress: () => navigation.pop(1),
-              },
-            ],
-          );
-        }
-      });
+                {
+                  text: 'OK',
+                  onPress: () => {
+                    console.log('provider Id');
+                    console.log(id);
+
+                    axios
+                      .post('http://10.0.2.2:5000/job', {
+                        jobType: jobType,
+                        description: description,
+                        requestedTime: requestedTime,
+                        longitude: longi,
+                        latitude: lat,
+                        providerId: id,
+                        consumerId: CID,
+                      })
+                      .then(function (response) {
+                        console.log(response.data);
+                        console.log(response.data._id);
+                        axios
+                          .post('http://10.0.2.2:5000/jobAssignment', {
+                            jobId: response.data._id,
+                            providerId: id,
+                          })
+                          .then(function (response) {
+                            console.log(response.data);
+                            navigation.navigate('PhotoUpload', {
+                              jobId: response.data.jobId,
+                            });
+                            // navigation.navigate('JobAcknowledge');
+                          })
+                          .catch(function (error) {
+                            console.log(error);
+                          });
+                      })
+                      .catch(function (error) {
+                        console.log(error);
+                      });
+                  },
+                },
+              ],
+            );
+          } else {
+            Alert.alert(
+              'Provider not available',
+              'We are sorry to inform that, your requested provider will not be available on your requested date. You can able to make schedule on another day',
+              [
+                {
+                  text: 'Cancel',
+                  onPress: () => console.log('Cancel Pressed'),
+                  style: 'cancel',
+                },
+                {
+                  text: 'Request another date',
+                  onPress: () => navigation.pop(1),
+                },
+              ],
+            );
+          }
+        });
+    } else {
+      Alert.alert(
+        'Sorry, your selected service provider is too far from your location',
+      );
+    }
+  };
 
   const handleView = (
     id,
@@ -165,7 +186,7 @@ const SearchProvider = ({navigation, route}) => {
         <Sbutton
           primary={true}
           text="Send Request"
-          onPress={() => AlertRequest(id)}
+          onPress={() => AlertRequest(id, providerLat, providerLongi)}
         />
         <Sbutton
           text="View Profile"
