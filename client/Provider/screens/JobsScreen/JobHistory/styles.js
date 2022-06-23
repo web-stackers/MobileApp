1f1887a4f05c41b12604951628f13fcf52e80193
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2022-06-23 07:57:06 +0530

    job screen copy from pavan

--- client/Provider/screens/JobsScreen/JobHistory/styles.js
+++ client/Provider/screens/JobsScreen/JobHistory/styles.js
@@ -1,32 +1,31 @@
+/* eslint-disable prettier/prettier */
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#121212',
-        width: '100%',
-        padding: 15
-    },
-    stateField: {
-        padding: 10,
-        marginBottom: 10,
-        height: 80,
-        backgroundColor: '#606465',
-        borderColor: '#3b3d3e',
-        borderWidth: 2,
-        borderRadius: 10,
-        display:'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between'      
-    },
-    state: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        lineHeight: 40,
-        marginLeft: 5
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    width: '100%',
+    padding: 15,
+  },
+  stateField: {
+    padding: 10,
+    marginBottom: 10,
+    height: 80,
+    backgroundColor: '#606465',
+    borderColor: '#3b3d3e',
+    borderWidth: 2,
+    borderRadius: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  state: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    lineHeight: 40,
+    marginLeft: 5,
+  },
 });
 
 export default styles;
-
-
--- client/consumer/screens/JobHistory/HistoryScreen/index.js
+++ client/consumer/screens/JobHistory/HistoryScreen/index.js
@@ -1,71 +1,67 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, Button, SafeAreaView} from 'react-native';
+import {View, SafeAreaView, TouchableWithoutFeedback} from 'react-native';
+import {Text} from 'react-native-paper';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import styles from './styles';
 
-const Separator = () => <View style={styles.separator} />;
+const JobState = ({state, onPress}) => {
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.stateField}>
+        <Text style={styles.state}>{state}</Text>
+        <MaterialCommunityIcons
+          name="arrow-right"
+          color="#fff"
+          size={40}
+          style={styles.icon}
+        />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
-const JobsHistory = ({navigation}) => {
+const JobHistory = ({navigation, userParams}) => {
+  const {type, _id} = userParams;
+
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Button
-          title="Completed Jobs"
-          color="#000000"
-          onPress={() => navigation.navigate('Completed Jobs')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Button
-          title="Pending Jobs"
-          color="#000000"
-          onPress={() => navigation.navigate('Pending Jobs')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Button
-          title="Refused Request"
-          color="#000000"
-          onPress={() => navigation.navigate('Refused Request')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Button
-          title="Withdrawn Jobs"
-          color="#000000"
-          onPress={() => navigation.navigate('Withdrawn Jobs')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Button
-          title="Request Sent"
-          color="#000000"
-          onPress={() => navigation.navigate('Request Sent')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Button
-          title="Pending Quotation"
-          color="#000000"
-          onPress={() => navigation.navigate('Pending Quotation')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Button
-          title="Quotation Rejected"
-          color="#000000"
-          onPress={() => navigation.navigate('Quotation Rejected')}
-        />
-      </View>
-      <Separator />
+      <JobState
+        state="Request Sent"
+        onPress={() => navigation.navigate('Request Sent')}
+      />
+      <JobState
+        state="Pending Quotation"
+        onPress={() => navigation.navigate('Pending Quotation')}
+      />
+      <JobState
+        state="Pending Jobs"
+        onPress={() => navigation.navigate('Pending Jobs')}
+      />
+      <JobState
+        state="Completed Jobs"
+        onPress={() =>
+          navigation.navigate('Completed Jobs', {
+            id: `${_id}`,
+            type: 'provider',
+          })
+        }
+      />
+      <JobState
+        state="Refused Request"
+        onPress={() => navigation.navigate('Refused Request')}
+      />
+      <JobState
+        state="Quotation Rejected"
+        onPress={() => navigation.navigate('Quotation Rejected')}
+      />
+      <JobState
+        state="Withdrawn Jobs"
+        onPress={() => navigation.navigate('Withdrawn Jobs')}
+      />
     </SafeAreaView>
   );
 };
 
-export default JobsHistory;
+export default JobHistory;
